3e15a4a81e6b857e2900e90b841d9da953ea9223
InfoGraphTheory <contact@infotriple.com>
2025-02-16 19:49:54 +0100

    rename create_desc_id2 to create_desc_id and delete the old create_desc_id function in descriptor_tools

--- src/misc/descriptor_tools.rs
+++ src/misc/descriptor_tools.rs
@@ -6,28 +6,13 @@ pub fn get_desc_id(desc: &Descriptor) -> String {
     create_desc_id(&desc.point, &desc.name, &desc.label, &desc.description)    
 }
 
-//Returns digest after a bigger procedure.
-//TODO: Consider if procedure should be out-phased in favor of function create_desc_id2 
-pub fn create_desc_id(point: &str, name: &str, label: &str, description: &str) -> String {
-
-    //hash fields with hash_text()
-    let point_hs = hashing_tools::hash_text(point);
-    let name_hs = hashing_tools::hash_text(name);
-    let label_hs = hashing_tools::hash_text(label);
-    let description_hs = hashing_tools::hash_text(description);
-    //concat fields with push_hashed()
-    let concat = hashing_tools::push_hashed(&point_hs, &name_hs, &label_hs, &description_hs);
-    //hash concat with hash_text()
-    hashing_tools::hash_text(&concat)
-}
-
 
 //return digest after a bigger concat procedure
 //This approach to id creation dictates that fields has to be trimmed and that a newline char
 //has to be used as delimitter and that no other fields than the description can have newline
 //chars inside their value. In that way we can easily split the fields but at the expense of
 //newline dictatorship. 
-pub fn create_desc_id2(point: &str, name: &str, label: &str, description: &str) -> String {
+pub fn create_desc_id(point: &str, name: &str, label: &str, description: &str) -> String {
 
     let mut concat: String = String::from("");
     concat.push_str(point.trim());
