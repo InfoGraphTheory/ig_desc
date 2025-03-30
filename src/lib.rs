b9661475f1e8a6760fbadbfd6daa82d875b1adc5
InfoGraphTheory <contact@infotriple.com>
2025-03-30 21:46:33 +0200

    Add domain vars and bump version

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ mod store;
 pub use logic::desc_director::DescDirector;
 pub use misc::descriptor_tools;
 pub use model::descriptor::Descriptor;
+pub use model::space::Space;
+pub use model::app::App;
 pub use service::desc_service_fs;
 pub use store::descriptor_facade;
 pub use store::descriptor_store;
